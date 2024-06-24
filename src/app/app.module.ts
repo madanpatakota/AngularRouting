96abba46d5c30a967b7d591c0fc72d11480214d6
Madn <madan.patakota@gmail.com>
2024-06-24 10:55:34 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { InstructionsComponent } from './instructions/instructions.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { CustomerOrdersComponent } from './customer-orders/customer-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InstructionsComponent,
+    OrderDetailsComponent,
+    CustomerOrdersComponent
   ],
   imports: [
     BrowserModule
