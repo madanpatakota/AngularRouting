ed995ba8da6682416900d6745b23afa2e5d58f7f
Madn <madan.patakota@gmail.com>
2024-06-25 09:01:16 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { InstructionsComponent } from './instructions/instructions.component';
 import { OrderDetailsComponent } from './order-details/order-details.component';
 import { CustomerOrdersComponent } from './customer-orders/customer-orders.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+
+
+//Logic imp 
+const routes : Routes = [
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'aboutus', component: AboutusComponent
+  }
+]
+
+// if path is aboutus then show aboutus component
+// if path is home then show home component
+
+// ---------
+// ------------
+
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +38,8 @@ import { CustomerOrdersComponent } from './customer-orders/customer-orders.compo
     CustomerOrdersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
